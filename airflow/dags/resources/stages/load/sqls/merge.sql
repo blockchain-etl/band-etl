892ac4af7f6635bf04a820cfc87dc8beb3150dad
medvedev1088 <medvedev1088@gmail.com>
2020-09-10 14:20:27 +0700

    Fix load dag

--- airflow/dags/resources/stages/load/sqls/merge.sql
+++ airflow/dags/resources/stages/load/sqls/merge.sql
@@ -1,7 +1,7 @@
 merge `{{destination_dataset_project_id}}.{{destination_dataset_name}}.{{table}}` dest
 using {{dataset_name_temp}}.{{table}} source
 on false
-when not matched and date(timestamp) = '{{ds}}' then
+when not matched and date(block_timestamp_truncated) = '{{ds}}' then
 insert (
     {% for column in table_schema %}
     {% if loop.index0 > 0 %},{% endif %}`{{ column.name }}`
@@ -11,5 +11,5 @@ insert (
     {% if loop.index0 > 0 %},{% endif %}`{{ column.name }}`
     {% endfor %}
 )
-when not matched by source and date(timestamp) = '{{ds}}' then
+when not matched by source and date(block_timestamp_truncated) = '{{ds}}' then
 delete
