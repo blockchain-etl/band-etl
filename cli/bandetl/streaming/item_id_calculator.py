f99beaf4617310eae00d6aadc642d43a9f9bac35
Worawat Wijarn <worawat.wijarn@gmail.com>
2020-09-13 22:27:48 +0700

    Add item id for all entity types

--- cli/bandetl/streaming/item_id_calculator.py
+++ cli/bandetl/streaming/item_id_calculator.py
@@ -34,6 +34,16 @@ class ItemIdCalculator:
 
         if item_type == 'oracle_request' and item.get('oracle_request_id') is not None:
             return concat(item_type, item.get('oracle_request_id'))
+        elif item_type == 'block' and item.get('block_hash') is not None:
+            return concat(item_type, item.get('block_hash'))
+        elif item_type == 'transaction' and item.get('txhash') is not None:
+            return concat(item_type, item.get('txhash'))
+        elif item_type == 'message' and item.get('txhash') is not None:
+            return concat(item_type, item.get('txhash'))
+        elif item_type == 'block_event' and item.get('block_height') is not None and item.get('index') is not None:
+            return concat(item_type, item.get('block_height'), item.get('index'))
+        elif item_type == 'log' and item.get('txhash') is not None and item.get('log_index') is not None:
+            return concat(item_type, item.get('txhash'), item.get('log_index'))
 
         logging.warning('item_id for item {} is None'.format(json.dumps(item)))
 
