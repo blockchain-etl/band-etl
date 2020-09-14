3bc2a753ff233aa17ef7631dff84e2c8ce334588
Worawat Wijarn <worawat.wijarn@gmail.com>
2020-09-14 07:26:13 +0700

    Bump version

--- cli/bandetl/cli/__init__.py
+++ cli/bandetl/cli/__init__.py
@@ -27,7 +27,7 @@ from bandetl.cli.get_block_range_for_date import get_block_range_for_date
 
 
 @click.group()
-@click.version_option(version='0.0.3')
+@click.version_option(version='0.0.4')
 @click.pass_context
 def cli(ctx):
     pass
